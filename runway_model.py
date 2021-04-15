c76b2042debd93d42e804d3ce891534cffc293ca
Marcio Puga <marciopuga@gmail.com>
2021-04-15 15:20:03 +1000

    Try version from example

--- runway_model.py
+++ runway_model.py
@@ -1,68 +1,13 @@
-# MIT License
-
-# Copyright (c) 2019 Runway AI, Inc
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-# =========================================================================
-
-# This example contains the minimum specifications and requirements
-# to port a machine learning model to Runway.
-
-# For more instructions on how to port a model to Runway, see the Runway Model
-# SDK docs at https://sdk.runwayml.com
-
-# RUNWAY
-# www.runwayml.com
-# hello@runwayml.com
-
-# =========================================================================
-
-# Import the Runway SDK. Please install it first with
-# `pip install runway-python`.
-import runway
-from runway.data_types import number, text, image, vector
 import pickle
 import numpy as np
 import tensorflow as tf
 import dnnlib.tflib as tflib
 import runway
 
-import re
-
-import dnnlib
-import dnnlib.tflib as tflib
-
-
 fmt = dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=True)
+
 np.random.seed(0)
 tf.random.set_random_seed(0)
-
-
-from example_model import ExampleModel
-
-# Setup the model, initialize weights, set the configs of the model, etc.
-# Every model will have a different set of configurations and requirements.
-# Check https://docs.runwayapp.ai/#/python-sdk to see a complete list of
-# supported configs. The setup function should return the model ready to be
-# used.
-
 
 @runway.setup(options={'checkpoint': runway.file(extension='.pkl')})
 def setup(opts):
@@ -70,10 +15,8 @@ def setup(opts):
     tflib.init_tf()
     with open(opts['checkpoint'], 'rb') as file:
         G, D, Gs = pickle.load(file)
-    # with open('model.pkl', 'rb') as file:
-    #     G, D, Gs = pickle.load(file)
-
     return Gs
+
 
 generate_inputs = {
     'z': runway.vector(512, sampling_std=0.5),
@@ -89,12 +32,6 @@ def convert(model, inputs):
     output = np.clip(images[0], 0, 255).astype(np.uint8)
     return {'image': output}
 
-    # output = PIL.Image.fromarray(image[0], 'RGB')
-
-    # return {'image': output}
-
 
 if __name__ == '__main__':
-    # run the model server using the default network interface and ports,
-    # displayed here for convenience
-    runway.run(host='0.0.0.0', port=8000)
+    runway.run()
